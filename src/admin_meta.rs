9082a7d9c7e8f8fb8f8750493574b35928275a73
tiannian <dtiannian@aliyun.com>
2020-05-07 17:12:12 +0800

    basic init of req and resp

---
+++ src/admin_meta.rs
@@ -0,0 +1,44 @@
+use actix_web::{web, HttpResponse, post, Responder, put, get};
+use serde::{Serialize, Deserialize};
+use crate::response::ResponseBody;
+
+// new meta
+
+#[post("/api/admin/meta")]
+pub async fn new_meta() -> impl Responder {
+    HttpResponse::Ok().json(ResponseBody::<()>::new_success(None))
+}
+
+// update meta
+
+#[derive(Deserialize, Debug)]
+pub struct UpdateMetaRequest {
+    seed: String,
+}
+
+#[put("/api/admin/meta")]
+pub async fn update_meta(req: web::Json<UpdateMetaRequest>) -> impl Responder {
+    println!("{:?}", req);
+    HttpResponse::Ok().json(ResponseBody::<()>::new_success(None))
+}
+
+// get meta
+
+#[derive(Serialize)]
+pub struct GetMetaResponse {
+    code: String,
+    public: String,
+    secret: String,
+    seed: String,
+}
+
+#[get("/api/admin/meta")]
+pub async fn get_meta() -> impl Responder {
+    HttpResponse::Ok().json(ResponseBody::new_success(Some(GetMetaResponse {
+        code: String::from(""),
+        public: String::from(""),
+        secret: String::from(""),
+        seed: String::from("")
+    })))
+}
+
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpServer};
+
+mod admin_meta;
+pub mod response;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(admin_meta::new_meta)
+            .service(admin_meta::update_meta)
+            .service(admin_meta::get_meta)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+}
