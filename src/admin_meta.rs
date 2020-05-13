f0d130dd5d9e5ff5046435548d1738d9fe3df19b
luo4lu <luo4lu@163.com>
2020-05-13 16:15:46 +0800

    temporary storage

--- src/admin_meta.rs
+++ src/admin_meta.rs
@@ -1,16 +1,40 @@
+use crate::config_path::ConfigPath;
 use crate::response::ResponseBody;
 use actix_web::{get, post, put, web, HttpResponse, Responder};
+use asymmetric_crypto::hasher::sha3::Sha3;
+use asymmetric_crypto::keypair::Keypair;
+use core::convert::AsRef;
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
+//use hex::FromHex;
+//use std::fmt::Write;
+
+use tokio::fs::File;
+use tokio::prelude::*;
 
 // new meta
 
 #[post("/api/admin/meta")]
-pub async fn new_meta() -> impl Responder {
-    HttpResponse::Ok().json(ResponseBody::<()>::new_success(None))
+pub async fn new_meta(config: web::Data<ConfigPath>) -> impl Responder {
+    //decline a rand number object
+    let mut rng = thread_rng();
+    //generate Serialize structure data
+    let info_form_rang = Keypair::<
+        [u8; 32],
+        Sha3,
+        dislog_hal_sm2::PointInner,
+        dislog_hal_sm2::ScalarInner,
+    >::generate(&mut rng)
+    .unwrap();
+    let serialized = serde_json::to_string(&info_form_rang).unwrap();
+    let mut file = File::create(&config.meta_path).await.unwrap();
+    match file.write_all(serialized.as_ref()).await {
+        Ok(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_success(None)),
+        Err(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error()),
+    }
 }
 
 // update meta
-
 #[derive(Deserialize, Debug)]
 pub struct UpdateMetaRequest {
     seed: String,
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use actix_web::{App, HttpServer};
 
 mod admin_meta;
 mod admin_quota;
+mod config_path;
 pub mod response;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .data(config_path::ConfigPath::default())
             .service(admin_meta::new_meta)
             .service(admin_meta::update_meta)
             .service(admin_meta::get_meta)
