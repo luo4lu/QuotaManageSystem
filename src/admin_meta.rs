16e31fc9ad108dba94c6f5a615c60cfe9d99d2d3
luo4lu <luo4lu@163.com>
2020-05-14 18:23:45 +0800

    interface respones message implement

--- src/admin_meta.rs
+++ src/admin_meta.rs
@@ -6,8 +6,8 @@ use asymmetric_crypto::keypair::Keypair;
 use core::convert::AsRef;
 use rand::thread_rng;
 use serde::{Deserialize, Serialize};
-//use hex::FromHex;
-//use std::fmt::Write;
+use hex::{FromHex, ToHex};
+use std::fmt::Write;
 
 use tokio::fs::File;
 use tokio::prelude::*;
@@ -41,9 +41,20 @@ pub struct UpdateMetaRequest {
 }
 
 #[put("/api/admin/meta")]
-pub async fn update_meta(req: web::Json<UpdateMetaRequest>) -> impl Responder {
-    println!("{:?}", req);
-    HttpResponse::Ok().json(ResponseBody::<()>::new_success(None))
+pub async fn update_meta(config: web::Data<ConfigPath>, req: web::Json<UpdateMetaRequest>) -> impl Responder {
+    let sd: [u8; 32] = FromHex::from_hex(&req.seed).unwrap();
+    let info_form_rang = Keypair::<
+    [u8; 32],
+    Sha3,
+    dislog_hal_sm2::PointInner,
+    dislog_hal_sm2::ScalarInner,
+>::generate_from_seed(sd).unwrap();
+let serialized = serde_json::to_string(&info_form_rang).unwrap();
+let mut file = File::create(&config.meta_path).await.unwrap();
+match file.write_all(serialized.as_ref()).await{
+    Ok(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_success(None)),
+    Err(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error()),
+}
 }
 
 // get meta
@@ -57,11 +68,39 @@ pub struct GetMetaResponse {
 }
 
 #[get("/api/admin/meta")]
-pub async fn get_meta() -> impl Responder {
+pub async fn get_meta(config: web::Data<ConfigPath>) -> impl Responder {
+    //read file
+    let mut file = File::open(&config.meta_path).await.unwrap();
+    //read json file to string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.unwrap();
+
+    //Deserialize to the specified data format
+    let deserialize: Keypair<
+        [u8; 32],
+        Sha3,
+        dislog_hal_sm2::PointInner,
+        dislog_hal_sm2::ScalarInner,
+        > = serde_json::from_str(&contents).unwrap();
+
+        //format conversion to string
+        let mut secret_str = String::new();
+        let mut code_str = String::new();
+        let mut seed_str = String::new();
+        for a in deserialize.get_secret_key().to_bytes().iter(){
+            write!(secret_str,"{:02x}",a).unwrap();
+        }
+        let  public_str = deserialize.get_public_key().to_bytes().encode_hex();
+        for a in deserialize.get_code().iter(){
+            write!(code_str,"{:02x}",a).unwrap();
+        }
+        for a in deserialize.get_seed().iter(){
+            write!(seed_str,"{:02x}",a).unwrap();
+        }
     HttpResponse::Ok().json(ResponseBody::new_success(Some(GetMetaResponse {
-        code: String::from(""),
-        public: String::from(""),
-        secret: String::from(""),
-        seed: String::from(""),
+        code: code_str,
+        public: public_str,
+        secret: secret_str,
+        seed: seed_str,
     })))
 }
--- src/config_path.rs
+++ src/config_path.rs
@@ -1,14 +1,12 @@
 #[derive(Clone)]
 pub struct ConfigPath {
     pub meta_path: String,
-    pub quota_path: String,
 }
 
 impl Default for ConfigPath {
     fn default() -> Self {
         Self {
             meta_path: String::from("./meta.json"),
-            quota_path: String::from("./quota.json"),
         }
     }
 }
