ba66c9e368e6fb553e2f3d72ab806f627f69fca5
luo4lu <luo4lu@163.com>
2020-05-15 16:49:11 +0800

    Optimize performance and error handling

--- src/admin_meta.rs
+++ src/admin_meta.rs
@@ -4,10 +4,9 @@ use actix_web::{get, post, put, web, HttpResponse, Responder};
 use asymmetric_crypto::hasher::sha3::Sha3;
 use asymmetric_crypto::keypair::Keypair;
 use core::convert::AsRef;
+use hex::{FromHex, ToHex};
 use rand::thread_rng;
 use serde::{Deserialize, Serialize};
-use hex::{FromHex, ToHex};
-use std::fmt::Write;
 
 use tokio::fs::File;
 use tokio::prelude::*;
@@ -19,15 +18,33 @@ pub async fn new_meta(config: web::Data<ConfigPath>) -> impl Responder {
     //decline a rand number object
     let mut rng = thread_rng();
     //generate Serialize structure data
-    let info_form_rang = Keypair::<
+    let info_form_rang = match Keypair::<
         [u8; 32],
         Sha3,
         dislog_hal_sm2::PointInner,
         dislog_hal_sm2::ScalarInner,
     >::generate(&mut rng)
-    .unwrap();
-    let serialized = serde_json::to_string(&info_form_rang).unwrap();
-    let mut file = File::create(&config.meta_path).await.unwrap();
+    {
+        Ok(s) => s,
+        Err(e) => {
+            println!("keypair conversion failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
+    let serialized = match serde_json::to_string(&info_form_rang) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("serialized to string failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
+    let mut file = match File::create(&config.meta_path).await {
+        Ok(f) => f,
+        Err(e) => {
+            println!("file create failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_file_error());
+        }
+    };
     match file.write_all(serialized.as_ref()).await {
         Ok(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_success(None)),
         Err(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error()),
@@ -41,20 +58,48 @@ pub struct UpdateMetaRequest {
 }
 
 #[put("/api/admin/meta")]
-pub async fn update_meta(config: web::Data<ConfigPath>, req: web::Json<UpdateMetaRequest>) -> impl Responder {
-    let sd: [u8; 32] = FromHex::from_hex(&req.seed).unwrap();
-    let info_form_rang = Keypair::<
-    [u8; 32],
-    Sha3,
-    dislog_hal_sm2::PointInner,
-    dislog_hal_sm2::ScalarInner,
->::generate_from_seed(sd).unwrap();
-let serialized = serde_json::to_string(&info_form_rang).unwrap();
-let mut file = File::create(&config.meta_path).await.unwrap();
-match file.write_all(serialized.as_ref()).await{
-    Ok(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_success(None)),
-    Err(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error()),
-}
+pub async fn update_meta(
+    config: web::Data<ConfigPath>,
+    req: web::Json<UpdateMetaRequest>,
+) -> impl Responder {
+    let sd: [u8; 32] = match FromHex::from_hex(&req.seed) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("32 byte from hex failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
+    let info_form_rang = match Keypair::<
+        [u8; 32],
+        Sha3,
+        dislog_hal_sm2::PointInner,
+        dislog_hal_sm2::ScalarInner,
+    >::generate_from_seed(sd)
+    {
+        Ok(s) => s,
+        Err(e) => {
+            println!("keypair conversion failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
+    let serialized = match serde_json::to_string(&info_form_rang) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("serialized to string failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
+    let mut file = match File::create(&config.meta_path).await {
+        Ok(f) => f,
+        Err(e) => {
+            println!("file create failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_file_error());
+        }
+    };
+    match file.write_all(serialized.as_ref()).await {
+        Ok(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_success(None)),
+        Err(_) => HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error()),
+    }
 }
 
 // get meta
@@ -70,33 +115,42 @@ pub struct GetMetaResponse {
 #[get("/api/admin/meta")]
 pub async fn get_meta(config: web::Data<ConfigPath>) -> impl Responder {
     //read file
-    let mut file = File::open(&config.meta_path).await.unwrap();
+    let mut file = match File::open(&config.meta_path).await {
+        Ok(f) => f,
+        Err(e) => {
+            println!("file open failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_file_error());
+        }
+    };
     //read json file to string
     let mut contents = String::new();
-    file.read_to_string(&mut contents).await.unwrap();
-
+    match file.read_to_string(&mut contents).await {
+        Ok(s) => s,
+        Err(e) => {
+            println!("read file to string failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
     //Deserialize to the specified data format
     let deserialize: Keypair<
         [u8; 32],
         Sha3,
         dislog_hal_sm2::PointInner,
         dislog_hal_sm2::ScalarInner,
-        > = serde_json::from_str(&contents).unwrap();
+    > = match serde_json::from_str(&contents) {
+        Ok(de) => de,
+        Err(e) => {
+            println!("Keypair conversion failed:{:?}", e);
+            return HttpResponse::Ok().json(ResponseBody::<()>::new_str_conver_error());
+        }
+    };
 
-        //format conversion to string
-        let mut secret_str = String::new();
-        let mut code_str = String::new();
-        let mut seed_str = String::new();
-        for a in deserialize.get_secret_key().to_bytes().iter(){
-            write!(secret_str,"{:02x}",a).unwrap();
-        }
-        let  public_str = deserialize.get_public_key().to_bytes().encode_hex();
-        for a in deserialize.get_code().iter(){
-            write!(code_str,"{:02x}",a).unwrap();
-        }
-        for a in deserialize.get_seed().iter(){
-            write!(seed_str,"{:02x}",a).unwrap();
-        }
+    //format conversion to string
+    let secret_str = deserialize.get_secret_key().to_bytes().encode_hex();
+    let code_str = deserialize.get_code().encode_hex();
+    let seed_str = deserialize.get_seed().encode_hex();
+    let public_str = deserialize.get_public_key().to_bytes().encode_hex();
+
     HttpResponse::Ok().json(ResponseBody::new_success(Some(GetMetaResponse {
         code: code_str,
         public: public_str,
